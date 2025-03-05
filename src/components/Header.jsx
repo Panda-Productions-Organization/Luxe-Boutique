c88d06ae832e53a8d3d736814b7d4cb5a3f25055
Elayabarathi <elayabarathiedison@gmail.com>
2025-03-05 21:17:22 +0530

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,12 @@ const Header = () => {
         <div className="bg-yellow-400 h-16 w-16 flex justify-center items-center">
           <a href="/">Icon</a>
         </div>
+
+        <div className="bg-blue-400 h-16 w-100 ml-10 flex items-center">
+          <div className="bg-white w-full h-12 rounded-4xl">
+            <input type="text" placeholder="Search" className="w-full h-full p-4 outline-none" />
+          </div>
+        </div>
       </div>
     </>
   );
