734d7b72d596c44a6e9e713f6bec969f54fd362b
Elayabarathi <elayabarathiedison@gmail.com>
2025-03-05 21:27:03 +0530

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { RiSearchLine } from "react-icons/ri";
+
 const Header = () => {
   return (
     <>
@@ -9,8 +11,15 @@ const Header = () => {
         </div>
 
         <div className="bg-blue-400 h-16 w-100 ml-10 flex items-center">
-          <div className="bg-white w-full h-12 rounded-4xl">
-            <input type="text" placeholder="Search" className="w-full h-full p-4 outline-none" />
+          <div className="bg-white w-full h-12 rounded-4xl flex items-center pl-4">
+            <div className="flex items-center justify-center w-6 h-6">
+              <RiSearchLine />
+            </div>
+            <input
+              type="text"
+              placeholder="Search for Products, Brands and More"
+              className="w-full h-full p-1 outline-none"
+            />
           </div>
         </div>
       </div>
