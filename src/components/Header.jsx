0c8b8ade4daaeb6cf75d1aa16f72890cbfbe9a77
Elayabarathi <elayabarathiedison@gmail.com>
2025-03-05 22:38:20 +0530

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
 import { RiSearchLine } from "react-icons/ri";
+import { FaRegCircleUser } from "react-icons/fa6";
+import { RiShoppingCartLine } from "react-icons/ri";
 
 const Header = () => {
   return (
@@ -17,8 +19,11 @@ const Header = () => {
 
         <div className="bg-blue-400 h-16 w-200 ml-10 flex items-center">
           <div className="bg-white w-full h-12 rounded-4xl flex items-center pl-4">
-            <div className="flex items-center justify-center w-6 h-6 bg-amber-400 cursor-pointer" onClick={() => console.log('search')}>
-              <RiSearchLine />
+            <div
+              className="flex items-center justify-center w-6 h-6 bg-amber-400 cursor-pointer"
+              onClick={() => console.log("search")}
+            >
+              <RiSearchLine className="size-4"/>
             </div>
             <input
               type="text"
@@ -29,14 +34,18 @@ const Header = () => {
         </div>
 
         <div className="bg-yellow-400 h-16 w-80 flex items-center p-6 gap-6">
-          <div className="bg-blue-400 h-12 w-12 flex items-center justify-center">Account</div>
-          <div className="bg-blue-400 h-12 w-12 flex items-center justify-center">Cart</div>
-          <button 
-          className="bg-sky-600 flex items-center justify-center rounded-2xl px-4 py-1 hover:bg-sky-700 active:bg-sky-600 cursor-pointer"
-          onClick={() => console.log('login')}
+          <div className="bg-sky-400 flex items-center justify-center p-1 rounded-full">
+            <FaRegCircleUser className="size-7 text-sky-950" />
+          </div>
+          <div className="bg-sky-400 flex items-center justify-center p-1 rounded-full">
+            <RiShoppingCartLine className="size-7 text-sky-950" />
+          </div>
+          <button
+            className="bg-sky-600 flex items-center justify-center rounded-2xl px-4 py-1 hover:bg-sky-700 active:bg-sky-600 cursor-pointer"
+            onClick={() => console.log("login")}
           >
             Login
-            </button>
+          </button>
         </div>
       </div>
     </>
