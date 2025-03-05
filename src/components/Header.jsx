98de9cb82cf76b6634852ab0b85cbc0f9b936868
Elayabarathi <elayabarathiedison@gmail.com>
2025-03-05 21:08:22 +0530

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,15 @@
-
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
     <>
       <div className="bg-red-400 h-20 w-full flex items-center p-2">
-        <div className="bg-yellow-400 h-16 w-16 flex justify-center items-center">Icon</div>
+        <div className="bg-yellow-400 h-16 w-16 flex justify-center items-center">
+          <a href="/">Icon</a>
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
