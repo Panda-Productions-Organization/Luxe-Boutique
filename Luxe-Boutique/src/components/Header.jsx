650b7daa0619216bfc3de698969e1b71c1436e6f
Elayabarathi <elayabarathiedison@gmail.com>
2025-03-06 14:45:30 +0530

    file tree change

--- Luxe-Boutique/src/components/Header.jsx
+++ Luxe-Boutique/src/components/Header.jsx
@@ -7,13 +7,16 @@ import { RiShoppingCartLine } from "react-icons/ri";
 const Header = () => {
   return (
     <>
-      <div className="bg-red-400 h-20 w-full flex items-center justify-between p-2">
+      <div className="bg-red-400 h-20 w-full flex items-center justify-between p-2 shadow-xl hover:shadow-2xl">
         <div className="flex gap-6">
           <div className="bg-yellow-400 h-16 w-16 flex justify-center items-center">
             <a href="/">Icon</a>
           </div>
           <div className="bg-yellow-400 h-16 w-30 flex justify-center items-center">
-            <a href="/">Company name</a>
+            <a href="/" className="flex flex-col items-center">
+              <p className="font-bold text-2xl font-sans">LUXE</p>
+              <p className="font-bold text-sm font-sans">Boutique</p>
+            </a>
           </div>
         </div>
 
@@ -23,7 +26,7 @@ const Header = () => {
               className="flex items-center justify-center w-6 h-6 bg-amber-400 cursor-pointer"
               onClick={() => console.log("search")}
             >
-              <RiSearchLine className="size-4"/>
+              <RiSearchLine className="size-4" />
             </div>
             <input
               type="text"
@@ -33,13 +36,15 @@ const Header = () => {
           </div>
         </div>
 
-        <div className="bg-yellow-400 h-16 w-80 flex items-center p-6 gap-6">
+        <div className="bg-yellow-400 h-16 w-80 flex items-center p-6 gap-6 ">
           <div className="bg-sky-400 flex items-center justify-center p-1 rounded-full">
             <FaRegCircleUser className="size-7 text-sky-950" />
           </div>
-          <div className="bg-sky-400 flex items-center justify-center p-1 rounded-full">
+          <div className="bg-sky-400 flex items-center justify-center p-1 rounded-full relative">
+            <div className="absolute bottom-4 right-8 text-mx font-semibold">0</div>
             <RiShoppingCartLine className="size-7 text-sky-950" />
           </div>
+
           <button
             className="bg-sky-600 flex items-center justify-center rounded-2xl px-4 py-1 hover:bg-sky-700 active:bg-sky-600 cursor-pointer"
             onClick={() => console.log("login")}
